45791f0d7f3197b4a6ff7a61ab7f5a139ebc695b
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-10-07 12:15:05 +0900

    Add eslint-tsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-const helloWorld = 'helloWorld';
-console.log(helloWorld);
+/**
+ * @deprecated Use the new {@link Control} base class instead.
+ */
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+console.log(add(1, 1));
